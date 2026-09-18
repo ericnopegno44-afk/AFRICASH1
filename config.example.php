23f17f2dc450b7843457c6b0bb26d8c6026d6661
ericnopegno44-afk <ericnopegno44@gmail.com>
2026-09-18 12:01:50 +0000

    Add files via upload

---
+++ config.example.php
@@ -0,0 +1,2 @@
+<?php
+return ['site_name'=>'AFRICASH','signup_fee'=>2500,'minimum_withdrawal'=>2000,'currency'=>'XOF','merchant_id'=>'','api_base_url'=>'','api_key'=>'','api_secret'=>'','webhook_secret'=>''];
---
+++ payment.php
@@ -0,0 +1,1 @@
+<?php header('Content-Type: application/json'); echo json_encode(['ok'=>false,'message'=>'Paiement réel non activé dans la version test.']);
